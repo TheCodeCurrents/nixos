6b74c1ec371a00153765b89c5cf4b426397b72ca
Jakob Flocke <jflocke@proton.me>
2025-09-12 20:49:48 +0200

    wifi

--- hosts/ideapad/configuration.nix
+++ hosts/ideapad/configuration.nix
@@ -15,7 +15,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jflocke = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel", "networkmanager" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
       networkmanager_dmenu  # dmenu-based Wi-Fi selector
