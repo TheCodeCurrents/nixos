e07912f33469fe82c85abdb4037f78e802e6648e
Jakob Flocke <jflocke@proton.me>
2025-09-20 00:35:38 +0200

    gnome

--- modules/core/common.nix
+++ modules/core/common.nix
@@ -18,7 +18,7 @@
 	services.xserver = {
 		enable = true;
 		layout = "de";
-		displayManager.gdm.enable = true;
+		# displayManager.gdm.enable = true;
 		desktopManager.gnome.enable = true;
 		autoRepeatDelay = 200;
 		autoRepeatInterval = 35;
