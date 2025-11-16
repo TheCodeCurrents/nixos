05d56e8f85b3517878fab03616add100f4a5fa8d
Jakob Flocke <jflocke@proton.me>
2025-11-16 14:29:19 +0100

    gnome setup

--- users/jflocke/gnome.nix
+++ users/jflocke/gnome.nix
@@ -9,6 +9,46 @@
   ];
 
   dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      disabled-extensions = "disabled";
+      enabled-extensions = [
+        "Vitals@CoreCoding.com"
+        "blur-my-shell@aunetx"
+        "gsconnect@andyholmes.github.io"
+      ];
+      favorite-apps = ["firefox.desktop" "alacritty.desktop" "org.gnome.Nautilus.desktop" "code.desktop"];
+    };
+    
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+      switch-group = ["<Alt>Tab"];
+      switch-group-backward = ["<Shift><Alt>Tab"];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      move-to-workspace-1 = ["<Shift><Super>1"];
+      move-to-workspace-2 = ["<Shift><Super>2"];
+      move-to-workspace-3 = ["<Shift><Super>3"];
+      move-to-workspace-4 = ["<Shift><Super>4"];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      next = [ "<Shift><Control>n" ];
+      previous = [ "<Shift><Control>p" ];
+      play = [ "<Shift><Control>space" ];
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "alacritty";
+      command = "alacritty";
+      binding = "<Super>Return";
+    };
+
     "org/gnome/desktop/background" = {
       picture-uri = "file:///home/jflocke/nixos/wallpapers/jellyfish.jpg";
       picture-uri-dark = "file:///home/jflocke/nixos/wallpapers/jellyfish.jpg";
@@ -16,13 +56,26 @@
     };
 
     "org/gnome/desktop/interface" = {
+      clock-show-seconds = true;
+      clock-show-weekday = true;
       color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      font-antialiasing = "grayscale";
+      font-hinting = "slight";
+      gtk-theme = "Nordic";
+      toolkit-accessibility = true;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      # button-layout = "close,minimize,maximize:appmenu";
+      num-workspaces = 10;
     };
 
     "org/gnome/desktop/screensaver" = {
       picture-uri = "file:///home/jflocke/nixos/wallpapers/jellyfish.jpg";
     };
 
+    # Extensions
     "org/gnome/shell/extensions/vitals" = {
       show-memory = true;
       show-cpu = true;
@@ -39,28 +92,6 @@
       show-storage = true;
       show-network = true;
       show-gpu = true;
-    };
-
-    "org/gnome/desktop/wm/keybindings" = {
-      switch-applications = ["<Super>Tab"];
-      switch-applications-backward = ["<Shift><Super>Tab"];
-      switch-group = ["<Alt>Tab"];
-      switch-group-backward = ["<Shift><Alt>Tab"];
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      move-to-workspace-1 = ["<Shift><Super>1"];
-      move-to-workspace-2 = ["<Shift><Super>2"];
-      move-to-workspace-3 = ["<Shift><Super>3"];
-      move-to-workspace-4 = ["<Shift><Super>4"];
-    };
-
-    "org/gnome/shell" = {
-      enabled-extensions = [
-        "Vitals@CoreCoding.com"
-        "blur-my-shell@aunetx"
-      ];
     };
   };
 }
