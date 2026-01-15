8d00c7b047d7573572e2c5a11c54633401e38bf8
Jakob Flocke <jflocke@proton.me>
2026-01-15 10:38:15 +0100

    tweaked some qemu related stuff

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,9 +1,10 @@
 { lib, pkgs, ... }:
 {
 
-  boot.kernelModules = [ "vboxdrv" "vboxnetflt" "vboxnetadp" ];
+  boot.kernelModules = [ "vboxdrv" "vboxnetflt" "vboxnetadp" "kvm-intel" ];
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.runAsRoot = false;
 
   # virtualisation.virtualbox.host.enable = true;
   # virtualisation.virtualbox.host.enableExtensionPack = true;
